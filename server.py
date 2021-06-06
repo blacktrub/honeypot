4d3087785e4df5f3593571257f8f4aab5135d3d6
blacktrub <lightlocdog@gmail.com>
2021-06-06 22:00:02 +0300

    add logs

--- server.py
+++ server.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from logging.config import dictConfig
+from logging import getLogger
+
 from flask import Flask, render_template, request, redirect, url_for
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    },
+    '': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
+logger = getLogger(__name__)
 
 
 @app.route('/admin', methods=['GET', 'POST'])
@@ -12,9 +35,11 @@ def admin_page():
     if request.method == 'POST':
         login = request.form['login']
         password = request.form['password']
+        logger.info('try with %s %s', dict(request.form), dict(request.headers))
         if login == 'admin' and password == 'admin':
             return render_template('success.html')
         else:
             return redirect(url_for('admin_page'))
+    logger.info('open with %s %s', dict(request.args), dict(request.headers))
     return render_template('admin.html')
 
